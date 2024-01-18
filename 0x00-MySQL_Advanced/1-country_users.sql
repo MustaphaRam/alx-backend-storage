182f6479233e302d1c07b494fa6b700e1fedb863
MustaphaRam <mustapha.mad.75@gmail.com>
2024-01-18 12:35:53 +0100

    /0x00-MySQL_Advanced

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -10,9 +10,9 @@ If the table already exists, your script should not fail
 Your script can be executed on any database
 */
 
-CREATE TABLE IF NOT EXISTS users (
-    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `email` varchar(255) NOT NULL UNIQUE,
-    `name` varchar(255) NOT NULL,
-    `country` ENUM('US','CO','TN') NOT NULL DEFAULT 'US'
+CREATE TABLE IF NOT EXISTS users(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255) NOT NULL,
+    country ENUM('US','CO','TN') DEFAULT 'US' NOT NULL
 );
