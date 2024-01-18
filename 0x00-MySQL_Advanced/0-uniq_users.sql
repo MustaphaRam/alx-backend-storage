0096080ef86e26f0610882c54111f5f60cc56875
MustaphaRam <mustapha.mad.75@gmail.com>
2024-01-18 12:33:26 +0100

    /0x00-MySQL_Advanced

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -12,5 +12,5 @@
 CREATE TABLE IF NOT EXISTS users (
     `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `email` varchar(255) NOT NULL UNIQUE,
-    `name` varchar(255) NOT NULL,
+    `name` varchar(255) NOT NULL
 )
