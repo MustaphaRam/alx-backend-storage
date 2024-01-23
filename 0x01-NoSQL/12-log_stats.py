2aad3d76cadfe6432b28fd1df3749e0e6a978acf
MustaphaRam <mustapha.mad.75@gmail.com>
2024-01-23 22:57:46 +0100

    0x01-NoSQL

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,39 +1,30 @@
 #!/usr/bin/env python3
 """
-Provide some stats about Nginx logs stored in MongoDB
-Database: logs, Collection: nginx, Display same as example
-first line: x logs, x number of documents in this collection
-second line: Methods
-5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-one line with method=GET, path=/status
+Aggregation
 """
+
 from pymongo import MongoClient
 
 
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-
-def log_stats(mongo_collection, option=None):
-    """
-    Prototype: def log_stats(mongo_collection, option=None):
-    Provide some stats about Nginx logs stored in MongoDB
-    """
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
-    print("Methods:")
-    for method in METHODS:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
-
-
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    print(f'{logs_collection.count_documents(filter={})} logs')
+    print('Methods:')
+    print(f'\tmethod GET: {logs_collection.count_documents( filter={"method": "GET"} )}')
+    print(f'\tmethod POST: {logs_collection.count_documents( filter={"method": "POST"} )}')
+    print(f'\tmethod PUT: {logs_collection.count_documents( filter={"method": "PUT"} )}')
+    print(f'\tmethod PATCH: {logs_collection.count_documents( filter={"method": "PATCH"} )}')
+    print(f'\tmethod DELETE: {logs_collection.count_documents( filter={"method": "DELETE"} )}')
+    print(f'{logs_collection.count_documents( filter={"method": "GET", "path": "/status"} )} status check')
+    print(f'IPs:')
+    ip_count = logs_collection.aggregate(
+        [
+            {"$match": {}},
+            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}},
+            {"$limit": 10}
+        ]
+    )
+    for ip in ip_count:
+        print(f'\t{ip.get("_id")}: {ip.get("count")}')
